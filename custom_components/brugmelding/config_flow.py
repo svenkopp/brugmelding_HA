894672665ba93e9d04e5a1c2dd18fa5043bad9fd
svenkopp <9916571+svenkopp@users.noreply.github.com>
2025-12-04 22:54:53 +0100

    Update config_flow.py

--- custom_components/brugmelding/config_flow.py
+++ custom_components/brugmelding/config_flow.py
@@ -1,25 +1,42 @@
 import aiohttp
 import voluptuous as vol
+
 from homeassistant import config_entries
+from homeassistant.core import callback
+
 from .const import DOMAIN, URL
 
-class BrugmeldingFlow(config_entries.ConfigFlow, domain=DOMAIN):
+
+class BrugmeldingConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
+    def __init__(self):
+        self._brug_map = {}
+
     async def async_step_user(self, user_input=None):
+        # Wanneer gebruiker een brug kiest
         if user_input is not None:
-            brug_selected = user_input["brug"]
-            brug_id = self._brug_map[brug_selected]
+            brug_naam = user_input["brug"]
+            brug_id = self._brug_map[brug_naam]
+
             return self.async_create_entry(
-                title=brug_selected,
-                data={"brug_id": brug_id, "brug_naam": brug_selected},
+                title=brug_naam,
+                data={"brug_id": brug_id, "brug_naam": brug_naam},
             )
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(URL) as resp:
-                bruggen = await resp.json()
+        # Bruggen ophalen
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(URL) as resp:
+                    bruggen = await resp.json()
+        except Exception:
+            return self.async_abort(reason="cannot_connect")
 
-        self._brug_map = {f"{b['naam']} ({b['id']})": b["id"] for b in bruggen}
+        # Lijst maken voor dropdown
+        self._brug_map = {
+            f"{b['naam']} ({b['id']})": b["id"]
+            for b in bruggen
+        }
 
         schema = vol.Schema({
             vol.Required("brug"): vol.In(list(self._brug_map.keys()))
