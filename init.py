2909210f0974016c245ace884235415bf8a5ac29
svenkopp <9916571+svenkopp@users.noreply.github.com>
2025-12-04 22:07:03 +0100

    Create init.py

---
+++ init.py
@@ -0,0 +1,10 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    hass.config_entries.async_setup_platforms(entry, ["sensor"])
+    return True
